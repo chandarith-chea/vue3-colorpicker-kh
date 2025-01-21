b5cbc34fa0b81e32e60944882fa3636a0d800ee6
Chandarith Chea <45872539+chandarith-chea@users.noreply.github.com>
2025-01-21 09:24:36 +0700

    fix: add new lang (KH)

--- packages/lang/index.ts
+++ packages/lang/index.ts
@@ -1,9 +1,11 @@
 import enUS from "./en-US";
 import zhCn from "./zh-CN";
+import kmKH from './km-KH';
 
-export type Lang = "ZH-cn" | "En";
+export type Lang = "ZH-cn" | "En" | "Kh";
 
 export const Local: { [K in Lang]: { [key: string]: string } } = {
   En: enUS,
   "ZH-cn": zhCn,
+  Kh: kmKH
 };
---
+++ packages/lang/km-KH.ts
@@ -0,0 +1,8 @@
+export default {
+  start: "ចាប់ផ្តើម",
+  end: "បញ្ចប់",
+  pure: "សុទ្ធ",
+  gradient: "ក្រាឌ្យង់",
+  linear: "បន្ទាត់ត្រង់",
+  radial: "រង្វង់មូល",
+};
